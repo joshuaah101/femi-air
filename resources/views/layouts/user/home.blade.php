6f9eedc45bc86c2dd1ec1a24ce216ec07393abf8
Joshua Oluwajare Faloye <dejavaproglife@gmail.com>
2021-07-24 00:42:53 -0700

    Frontend => just concluded the active status of the dashboard menu

--- resources/views/layouts/user/home.blade.php
+++ resources/views/layouts/user/home.blade.php
@@ -1,4 +1,4 @@
-{{-- {{ dd(Request::path()) }} --}}
+{{-- {{ dd($menuUrl) }} --}}
 @extends('master')
 
 @section('title')
@@ -27,8 +27,8 @@
             </div>
             {{-- sidebar items --}}
             <div class="py-2 px-2 text-blue-800 space-y-1 mt-5">
-                <a href="?menu=dash" class="{{ request()->is('user/*') ? 'shadow-xl bg-blue-700 text-blue-100 hover:bg-white rounded rounded-r-full hover:shadow-md' : ''}}
-                    text-sm px-5 py-2 font-semibold  hover:text-blue-900 flex items-center">
+                <a href="?menu=dash" class="{{ $menuUrl == 'dash' ? 'shadow-xl bg-blue-700 text-blue-100 hover:bg-white hover:shadow-md' : ''}}
+                    text-sm px-5 py-2 font-semibold hover:bg-blue-100 hover:text-blue-900 flex items-center rounded rounded-r-full">
                     <span class="mr-2">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 13v-1m4 1v-3m4 3V8M8 21l4-4 4 4M3 4h18M4 4h16v12a1 1 0 01-1 1H5a1 1 0 01-1-1V4z" />
@@ -36,8 +36,8 @@
                     </span>
                     Dashboard
                 </a>
-                <a href="#" class="{{ request()->is('active/*') ? 'shadow-xl rounded bg-gray-100 hover:bg-white  hover:shadow-md' : ''}}
-                    text-sm px-5 py-2 font-semibold hover:bg-blue-100 hover:text-blue-900 flex items-center">
+                <a href="?menu=active" class="{{ $menuUrl == 'active' ? 'shadow-xl bg-blue-700 text-blue-100 hover:bg-white  hover:shadow-md' : ''}}
+                    text-sm px-5 py-2 font-semibold hover:bg-blue-100 hover:text-blue-900 flex items-center rounded rounded-r-full">
                     <span class="mr-2">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11.933 12.8a1 1 0 000-1.6L6.6 7.2A1 1 0 005 8v8a1 1 0 001.6.8l5.333-4zM19.933 12.8a1 1 0 000-1.6l-5.333-4A1 1 0 0013 8v8a1 1 0 001.6.8l5.333-4z" />
@@ -45,8 +45,8 @@
                     </span>
                     View active
                 </a>
-                <a href="?menu=history" class="{{ request()->is('?history') ? 'shadow-xl rounded bg-gray-100 hover:bg-white  hover:shadow-md' : ''}}
-                    text-sm px-5 py-2 font-semibold hover:bg-blue-100 hover:text-blue-900 flex items-center">
+                <a href="?menu=history" class="{{ $menuUrl == 'history' ? 'shadow-xl bg-blue-700 text-blue-100 hover:bg-white  hover:shadow-md' : ''}}
+                    text-sm px-5 py-2 font-semibold hover:bg-blue-100 hover:text-blue-900 flex items-center rounded rounded-r-full">
                     <span class="mr-2">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 5v2m0 4v2m0 4v2M5 5a2 2 0 00-2 2v3a2 2 0 110 4v3a2 2 0 002 2h14a2 2 0 002-2v-3a2 2 0 110-4V7a2 2 0 00-2-2H5z" />
@@ -54,8 +54,8 @@
                     </span>
                     Ticket history
                 </a>
-                <a href="?menu=profile" class="{{ request()->is('profile/*') ? 'shadow-xl rounded bg-gray-100 hover:bg-white  hover:shadow-md' : ''}}
-                    text-sm px-5 py-2 font-semibold hover:bg-blue-100 hover:text-blue-900 flex items-center">
+                <a href="?menu=profile" class="{{ $menuUrl == 'profile' ? 'shadow-xl bg-blue-700 text-blue-100 hover:bg-white  hover:shadow-md' : ''}}
+                    text-sm px-5 py-2 font-semibold hover:bg-blue-100 hover:text-blue-900 flex items-center rounded rounded-r-full">
                     <span class="mr-2">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
@@ -63,8 +63,8 @@
                     </span>
                     Profile
                 </a>
-                <a href="#" class="{{ request()->is('menu/*') ? 'shadow-xl rounded bg-gray-100 hover:bg-white  hover:shadow-md' : ''}}
-                    text-sm px-5 py-2 font-semibold hover:bg-blue-100 hover:text-blue-900 flex items-center">
+                <a href="?menu=settings" class="{{ $menuUrl == 'settings' ? 'shadow-xl bg-blue-700 text-blue-100 hover:bg-white  hover:shadow-md' : ''}}
+                    text-sm px-5 py-2 font-semibold hover:bg-blue-100 hover:text-blue-900 flex items-center rounded rounded-r-full">
                     <span class="mr-2">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
@@ -73,7 +73,7 @@
                     </span>
                     Settings
                 </a>
-                <a href="#" class="text-sm px-5 py-2 font-semibold text-red-500 flex items-center">
+                <a href="logout" class="text-sm px-5 py-2 font-semibold text-red-500 hover:text-red-400 flex items-center">
                     <span class="mr-2">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
