f89f98defd27bc22f99db5a03d1e269ea4b685a0
Joshua Oluwajare Faloye <dejavaproglife@gmail.com>
2021-07-24 01:46:37 -0700

    Frontend => just concluded the active status of the dashboard menu

--- resources/views/layouts/user/home.blade.php
+++ resources/views/layouts/user/home.blade.php
@@ -73,7 +73,7 @@
                     </span>
                     Settings
                 </a>
-                <a href="logout" class="text-sm px-5 py-2 font-semibold text-red-500 hover:text-red-400 flex items-center">
+                <a href="logout" class="text-sm px-5 py-2 font-semibold text-red-700 hover:text-red-600 hover:bg-red-100 rounded rounded-r-full flex items-center">
                     <span class="mr-2">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
